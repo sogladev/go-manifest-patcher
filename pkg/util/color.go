820fe0a15bdf8920d7728ed38def77b437d74e0e
Jelle Meeus <sogladev@gmail.com>
2025-01-30 07:21:42 +0100

    fix: disable colors in windows as they don't show correctly
    not imporing a third party library just for windows...

--- pkg/util/color.go
+++ pkg/util/color.go
@@ -1,5 +1,7 @@
 package util
 
+import "runtime"
+
 // ANSI color codes
 const (
 	Reset  = "\033[0m"
@@ -11,22 +13,37 @@ const (
 
 // Colorize wraps the given text with the specified color code
 func Colorize(text, color string) string {
+	if runtime.GOOS == "windows" {
+		return text // Return plain text on Windows
+	}
 	return color + text + Reset
 }
 
 // Color functions for convenience
 func ColorGreen(text string) string {
+	if runtime.GOOS == "windows" {
+		return text // Return plain text on Windows
+	}
 	return Colorize(text, Green)
 }
 
 func ColorYellow(text string) string {
+	if runtime.GOOS == "windows" {
+		return text // Return plain text on Windows
+	}
 	return Colorize(text, Yellow)
 }
 
 func ColorRed(text string) string {
+	if runtime.GOOS == "windows" {
+		return text // Return plain text on Windows
+	}
 	return Colorize(text, Red)
 }
 
 func ColorCyan(text string) string {
+	if runtime.GOOS == "windows" {
+		return text // Return plain text on Windows
+	}
 	return Colorize(text, Cyan)
 }
